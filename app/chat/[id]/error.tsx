eae59fb42bf7ff05d414bb3d984cc2f64ffcc9e1
Shema Elisa <shemaelyssa@gmail.com>
2024-09-29 13:12:23 +0200

    dark mode

---
+++ app/chat/[id]/error.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { ShieldAlert } from "lucide-react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className={"flex justify-center  mt-40"}>
+      <div className="flex flex-col w-[42vw] gap-4">
+        <div className="flex flex-col gap-2">
+          <Alert variant={"destructive"}>
+            <AlertTitle className={"rounded-md flex justify-center"}>
+              <ShieldAlert size={60} />
+            </AlertTitle> 
+            <AlertDescription className={"mt-3 flex justify-center"}>
+              <span className={"text-md"}>
+                {error?.message || "something went wrong"}
+              </span>
+            </AlertDescription>
+          </Alert>
+          <Button onClick={() => reset()} variant={"outline"}>
+            Retry
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
