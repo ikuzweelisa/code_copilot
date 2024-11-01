9f82a75c710e02e709a16bbb86cd72bf58b49130
Shema Elisa <shemaElyssa@gmail.com>
2024-11-01 20:55:47 +0000

    feat:streaming

--- lib/actions/chat/actions.tsx
+++ lib/actions/chat/actions.tsx
@@ -17,70 +17,68 @@ import { ClientMessage } from "@/lib/types";
 import { CoreMessage } from "ai";
 import { sleep } from "@/lib/utils";
 
-let textStream: undefined | ReturnType<typeof createStreamableValue<string>>;
-let textNode: undefined | React.ReactNode;
 export async function submitMessage(
   userMessage: string
 ): Promise<ClientMessage> {
-  
-    const message = `\You are a highly capable programming assistant.
+  const message = `\You are a highly capable programming assistant.
        If auser ask anything not related to programming , respond saying that you are a Programming assistant you cannot do that.and suggest what you can assist them,
       if a user  impossible tasks such as Running codes and other programming tasks  you are not capable , respond Saying that the This feature is currently unavailable and may added in the future.
       your answers should be well explained.
     `;
-    const state = getMutableAIState<typeof AIProvider>();
+  const state = getMutableAIState<typeof AIProvider>();
 
-    state.update({
-      ...state.get(),
-      messages: [
-        ...state.get()?.messages,
-        {
-          id: crypto.randomUUID(),
-          role: "user",
-          content: userMessage,
-        },
-      ],
-    });
-    const result = await streamUI({
-      model: google("gemini-1.5-flash-latest"),
-      initial: <SpinnerMessage />,
-      system: message,
-      messages: [
-        ...state.get()?.messages.map((message) => ({
-          role: message.role,
-          content: message.content,
-          id: message.id,
-        })),
-      ] as CoreMessage[],
-      text: async ({ done, content, delta }) => {
-        if (!textStream) {
-          textStream = createStreamableValue("");
-          textNode = <BotMessage>{textStream.value}</BotMessage>;
-        }
-        if (done) {
-          textStream.done();
-          state.done({
-            ...state.get(),
-            messages: [
-              ...state.get()?.messages,
-              {
-                role: "assistant",
-                id: crypto.randomUUID(),
-                content: content,
-              },
-            ],
-          });
-        } else {
-          textStream.update(delta);
-        }
-       return textNode  
+  let textStream: undefined | ReturnType<typeof createStreamableValue<string>>;
+  let textNode: undefined | React.ReactNode;
+  state.update({
+    ...state.get(),
+    messages: [
+      ...state.get()?.messages,
+      {
+        id: crypto.randomUUID(),
+        role: "user",
+        content: userMessage,
       },
-      
-    });
+    ],
+  });
+  const result = await streamUI({
+    model: google("gemini-1.5-flash-latest"),
+    initial: <SpinnerMessage />,
+    system: message,
+    messages: [
+      ...state.get()?.messages.map((message) => ({
+        role: message.role,
+        content: message.content,
+        id: message.id,
+      })),
+    ] as CoreMessage[],
+    text: async ({ done, content, delta }) => {
+      if (!textStream) {
+        textStream = createStreamableValue("");
+        textNode = <BotMessage>{textStream.value}</BotMessage>;
+      }
+      if (done) {
+        textStream.done();
+        state.done({
+          ...state.get(),
+          messages: [
+            ...state.get()?.messages,
+            {
+              role: "assistant",
+              id: crypto.randomUUID(),
+              content: content,
+            },
+          ],
+        });
+      } else {
+        textStream.update(delta);
+      }
+      return textNode;
+    },
+  });
 
-    return {
-      id: crypto.randomUUID(),
-      role: "assistant",
-      display: result.value,
-    };
+  return {
+    id: crypto.randomUUID(),
+    role: "assistant",
+    display: result.value,
+  };
 }
--- lib/hooks/use-stream.tsx
+++ lib/hooks/use-stream.tsx
@@ -1,25 +1,27 @@
-import { readStreamableValue, StreamableValue } from "ai/rsc";
+import { StreamableValue, readStreamableValue } from "ai/rsc";
 import { useEffect, useState } from "react";
 
-function useStreamText(content: string | StreamableValue<string>) {
-  const [text, setText] = useState<string>(
+export const useStreamableText = (
+  content: string | StreamableValue<string>
+) => {
+  const [rawContent, setRawContent] = useState(
     typeof content === "string" ? content : ""
   );
+
   useEffect(() => {
     (async () => {
       if (typeof content === "object") {
         let value = "";
         for await (const delta of readStreamableValue(content)) {
           if (typeof delta === "string") {
-        
-            setText((value = value + delta));
+            setRawContent((value => value + delta));
           }
         }
       }
     })();
   }, [content]);
-  return text
-}
 
+  return rawContent;
+};
 
-export default useStreamText
+export default useStreamableText;
